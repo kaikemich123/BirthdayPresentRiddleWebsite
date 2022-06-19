c356f3c90c0cdfadd4d9d34d920094d99ad7e4c4
Kai <37674207+kaikemich123@users.noreply.github.com>
2022-06-19 09:22:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function submitAnswer() {
     var input = document.getElementById("answerbox");
     var answerfeed = document.getElementById("answerfeed");
-    if (input.value.toLowerCase() === "schaduw") {
+    if (input.value.toLowerCase() === "handdoek") {
         answerfeed.innerHTML = "Yayy u did it maar er is nog 1 raadsel haha\n\n<b>Volgend raadsel: </b>\nKf lsjkhu ef wpmhfoef dmvf bmt kf nf tuvvsu pq jotubhsbn xbbs ifu gjdujfwf qfstpobhf Njob Ibslfs jo wpps lpnu."
     } else {
         answerfeed.innerHTML = answerfeed.innerHTML + "Verkeerd lmaooooooo\n\n"
